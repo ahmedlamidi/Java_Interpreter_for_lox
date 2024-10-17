721e72b63243fcaf7388fe4bc6278afc55b699af
ahmedlamidi <ahmedlamidi@usf.edu>
2024-10-17 03:18:37 -0400

    Added helper functions and created grammar for equalities and expressions

---
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,72 @@
+package com.craftinginterpreters.lox;
+
+import java.util.List;
+import static com.craftinginterpreters.lox.TokenType.*;
+
+class Parser {
+    private final List<Token> tokens; // these are the tokens we are going to parse
+    private int current = 0;
+    Parser(List<Token> tokens){
+        this.tokens = tokens; // initialize with a list of tokens
+    }
+
+    private Expr expression(){
+        // grammar is expression -> equality
+        return equality(); // an expression is equivalent to an equality right now
+    }
+
+    private Expr equality(){
+        // grammar is equality -> comparison ( ("!=" | "==") comparison) *;
+        Expr expr = comparison();
+        while(match(BANG_EQUAL, BANG)){
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        // this is left associative equality since it would evaluate left side first
+        return expr;
+    }
+
+    private Expr comparison(){
+        // grammar is term (( ">" | ">=" | "<" | "<=" ) term )*;
+        Expr expr = term();
+
+        //to do
+        return
+    }
+
+    private boolean match(TokenType... types){
+        // function to match the current token to any in the list of types
+        for (TokenType type : types){
+            if (check(type)){
+                advance();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean check(TokenType type){
+        // if we are end it is automatically false , return if the current type is the correct type
+        if (isAtEnd()) return false;
+        return peek().type == type;
+    }
+
+    private Token advance(){
+        if(!isAtEnd()) current ++;
+        return previous();
+    }
+
+    private boolean isAtEnd(){
+        return peek().type == EOF;
+    }
+
+    private Token peek(){
+        return tokens.get(current);
+    }
+
+    private Token previous(){
+        return tokens.get(current - 1);
+    }
+
+}
