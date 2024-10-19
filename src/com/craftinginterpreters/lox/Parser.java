ae8fc0bb4a864ec26c9852026f6afdc82b128c30
ahmedlamidi <ahmedlamidi@usf.edu>
2024-10-19 14:59:06 -0400

    Added parsing for different expressions

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -30,9 +30,63 @@ class Parser {
     private Expr comparison(){
         // grammar is term (( ">" | ">=" | "<" | "<=" ) term )*;
         Expr expr = term();
+        while(match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)){
+            Token operator = previous();
+            Expr right = term();
+            expr = new Expr.Binary(expr, operator, right); // it is in binary form
+        }
+        return expr;
+    }
 
-        //to do
-        return
+    private Expr term(){
+        // grammar is factor (("+" | "-") factor)*;
+        Expr expr = factor();
+        while(match(PLUS, MINUS)){
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr factor(){
+        // grammar is unary(("/" | "*") unary)*;
+        Expr expr = unary();
+        while(match(SLASH, STAR)){
+            Token operator = previous();
+            Expr right = unary();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr unary(){
+        // grammar is ("!" | "-") unary | primary
+        if (match(BANG, MINUS)){
+            Token operator = previous();
+            Expr right = unary();
+            return new Expr.Unary(operator, right);
+        }
+        return primary();
+    }
+
+    private Expr primary(){
+        // grammar is Number | String | "true" | "false" | "nil" | ( expression )
+        if (match(FALSE)) return new Expr.Literal(false);
+        if (match(TRUE)) return new Expr.Literal(true);
+        if (match(NIL)) return new Expr.Literal(null);
+
+        if (match(STRING, NUMBER)){
+            return new Expr.Literal(previous().literal);
+        }
+        if (match(LEFT_PAREN)){
+            Expr expr = expression();
+            // we need to match the end of the expression with a right parentheses
+            consume(RIGHT_PAREN, "expected right parentheses");
+            // to do add a function to check for a type and return an error message
+            return new Expr.Grouping(expr);
+        }
+        return null;
     }
 
     private boolean match(TokenType... types){
